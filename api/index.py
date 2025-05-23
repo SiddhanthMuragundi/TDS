aa8d846ac8a38332e163a5ee3305e89d78118fca
Siddhanth Muragundi <22f1000662@ds.study.iitm.ac.in>
2025-05-23 16:52:15 +0530

    Your commit message here

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from typing import List
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import json
@@ -13,14 +14,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-file_path = os.path.join(os.path.dirname(__file__), "q-vercel-python.json")
+file_path = "q-vercel-python.json"
 with open(file_path, "r") as f:
     student_records = json.load(f)
 
 student_data = {student["name"]: student["marks"] for student in student_records}
 
 @app.get("/")
-def get_marks(request: Request):
-    names = request.query_params.getlist("name")
-    marks = [student_data.get(name, None) for name in names]
+def get_marks(name: List[str] = Query([])):
+    marks = [student_data.get(n, None) for n in name]
     return JSONResponse(content={"marks": marks})
